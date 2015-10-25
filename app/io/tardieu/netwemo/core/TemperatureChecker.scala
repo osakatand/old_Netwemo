4550c1032e5293cd363c558b42490a54d601b333
Jacob Tardieu <jacob@content-square.fr>
2015-10-25 11:34:54 +0100

    Add cold hours for temperature

--- app/io/tardieu/netwemo/core/TemperatureChecker.scala
+++ app/io/tardieu/netwemo/core/TemperatureChecker.scala
@@ -5,8 +5,10 @@ import java.util.concurrent.TimeUnit
 import akka.actor.Cancellable
 import com.typesafe.config.ConfigFactory
 import connectors.WemoConnector
+import org.joda.time.LocalTime
 import play.api.Logger
 import play.api.libs.concurrent.Akka._
+import com.github.nscala_time.time.Imports._
 import play.api.Play.current
 
 import scala.concurrent.Future
@@ -24,38 +26,54 @@ class TemperatureChecker(getValueFunction: () => Future[Float], wemoConnector: W
   // Those values should be read from the database
   private var lowThreshold = conf.getDouble("lowThreshold")
   private var highThreshold = conf.getDouble("highThreshold")
+  private var coldLowThreshold = conf.getDouble("coldLowThreshold")
+  private var coldHighThreshold = conf.getDouble("coldHighThreshold")
+  private var coldStartTime: LocalTime = new LocalTime(conf.getInt("coldHourStart"), conf.getInt("coldMinuteStart"))
+  private var coldStopTime: LocalTime = new LocalTime(conf.getInt("coldHourStop"), conf.getInt("coldMinuteStop"))
   private var checkInterval = FiniteDuration(10, TimeUnit.MINUTES)
 
   private var schedule: Cancellable = _
 
-  /**
-   * Not implemented yet.
-   * Reads values from database and updates class variables.
-   */
-  def updateValues(): Unit = {
-    lowThreshold = lowThreshold
-    highThreshold = highThreshold
-    checkInterval = checkInterval
+  schedule = system.scheduler.schedule(FiniteDuration(1, TimeUnit.MINUTES), checkInterval)(runCheck(getValueFunction))
+
+  private def isWorkingDay: Boolean = {
+    LocalDateTime.now.getDayOfWeek match {
+      case 6 | 7 => false
+      case _ => true
+    }
   }
 
-  schedule = system.scheduler.schedule(FiniteDuration(1, TimeUnit.MINUTES), checkInterval)(runCheck(getValueFunction))
+  private def inColdHours(startTime: LocalTime, stopTime: LocalTime): Boolean = {
+    val now = LocalTime.now()
+    val b = (now > startTime) && (now < stopTime) && (isWorkingDay)
+    Logger.debug(s"In cold hours: $b")
+    b
+  }
 
   /**
    * Checks and switches the switch on or off
    * @param getValue the function providing the metric value
    */
   def runCheck(getValue: () => Future[Float]): Unit = {
-      val futureValue = getValue()
-      futureValue.foreach { value =>
-        if (value > highThreshold) {
-          Logger.debug(s"Temperature: $value > high threshold $highThreshold, switching off.")
-          wemoConnector.switchOff("chauffage")
-        }
-        if (value < lowThreshold) {
-          Logger.debug(s"Temperature: $value < low threshold $lowThreshold, switching on.")
-          wemoConnector.switchOn("chauffage")
-        }
+    val futureValue = getValue()
+    inColdHours(coldStartTime, coldStopTime) match {
+      case true => setState(futureValue, coldLowThreshold, coldHighThreshold)
+      case false => setState(futureValue, lowThreshold, highThreshold)
     }
   }
+
+  private def setState(futureValue: Future[Float], lowThreshold: Double, highThreshold: Double): Unit = {
+    futureValue.foreach { value =>
+      if (value > highThreshold) {
+        Logger.debug(s"Temperature: $value > high threshold $highThreshold, switching off.")
+        wemoConnector.switchOff("chauffage")
+      }
+      if (value < lowThreshold) {
+        Logger.debug(s"Temperature: $value < low threshold $lowThreshold, switching on.")
+        wemoConnector.switchOn("chauffage")
+      }
+    }
+  }
+
 }
 
